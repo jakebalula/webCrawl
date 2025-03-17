4195dc18b599bcacd63351e314b5a2790c22fad5
jakebalula <balula@hartford.edu>
2025-03-17 14:08:21 -0400

    added the extractLinks function
    Next will be to add concurrency

--- go.mod
+++ go.mod
@@ -1,3 +1,10 @@
 module webCrawl
 
-go 1.23
+go 1.23.0
+
+toolchain go1.23.6
+
+require (
+	golang.org/x/net v0.37.0 // indirect
+	golang.org/x/text v0.23.0 // indirect
+)
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
   "golang.org/x/net/html"
   "io"
   "net/http"
+  "strings"
 )
 
 // fetchURL sends an HTTP Get request and returns the response body as a string.
@@ -22,7 +23,26 @@ func fetchUrl(url string) (string, error) {
   return string(body), nil
 }
 
-func extractLinks(htmlContent string) []string {}
+func extractLinks(htmlContent string) []string {
+  var links []string
+  tokenizer := html.NewTokenizer(strings.NewReader(htmlContent))
+  for {
+    tt := tokenizer.Next()
+    switch tt {
+    case html.ErrorToken:
+      return links
+    case html.StartTagToken:
+      token := tokenizer.Token()
+      if token.Data == "a" {
+        for _, attr := range token.Attr {
+          if attr.Key == "href" {
+            links = append(links, attr.Val)
+          }
+        }
+      }
+    }
+  }
+}
 
 func main() {
   url := "https://github.com/jakebalula"
@@ -31,6 +51,10 @@ func main() {
     fmt.Println("Error fetching URL:", err)
     return
   }
-
+  links := extractLinks(content)
+  fmt.Println("Extracted links:")
+  for _, link := range links {
+    fmt.Println(link)
+  }
   fmt.Println("Page content:", content[:500]) //This prints the first 500 characters
 }
